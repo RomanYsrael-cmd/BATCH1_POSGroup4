c56682698287ee923659de5eab3c0b3b7cd58613
RomanYsrael-cmd <roman.ysrael.g.alba@gmail.com>
2025-06-20 00:40:03 +0800

    Added Discount Manger UI

---
+++ src/Batch1_POSG4/controller/DiscountController.java
@@ -0,0 +1,43 @@
+package Batch1_POSG4.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class DiscountController {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private TableView<?> tblDiscountTable;
+
+    @FXML
+    private TextField txtAmmount;
+
+    @FXML
+    private TextField txtDiscountCode;
+
+    @FXML
+    private ComboBox<?> txtItem;
+
+    @FXML
+    private ComboBox<?> txtType;
+
+    @FXML
+    void handlesCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handlesSubmit(ActionEvent event) {
+
+    }
+
+}
